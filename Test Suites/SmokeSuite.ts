69c72cef94a9b2c49cc32d79c553c67a440aa5ab
suyog <suyog@DESKTOP-VCILI1A>
2025-01-11 22:15:03 +0530

    TC and Test suites created

---
+++ Test Suites/SmokeSuite.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>SmokeSuite for git</description>
+   <name>SmokeSuite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>961221d6-07a6-4cfa-9a42-4e1dbd2e1638</testSuiteGuid>
+   <testCaseLink>
+      <guid>e71a498c-076c-442f-8e31-b6c589f006d8</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC1</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
